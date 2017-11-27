857d2090c70a065dd969b4b4049ddf05d9616dbf
522yaoyao	email = 1094131083@qq.com <1094131083@qq.com>
2017-11-27 23:06:02 +0800

    线程
    join

--- src/thread/SleepTest.java
+++ src/thread/SleepTest.java
@@ -20,19 +20,21 @@ public class SleepTest {
         //t.interrupt(); //这种结束方式比较粗暴，如果t线程打开了某个资源还没来得及关闭也就是run方法还没有执行完就强制结束线程，会导致资源无法关闭
         //t.stop();也是结束某个线程，这种方式比interrupt()更粗暴
         r.flag = false;
+        System.out.println("当前线程是主线程"+new Date());
     }
 }
 class Runnable1 implements Runnable{
     boolean flag = true; //用这种方式结束线程很不错，用一个变量控制run方法什么时候不再执行，不会出现run方法没有执行完毕就结束
+    int i=0;
     @Override
     public void run() { //run方法一结束，整个线程就终止了
-        while(flag){
-            System.out.println("---"+new Date()+"---");
+      //  while(flag){
+          System.out.println("当前线程的子线程"+" "+i+++" "+new Date()+"---");
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 return;
             }
+        //}
         }
-    }
 }
